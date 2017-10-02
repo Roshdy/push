64b492b461e69c287c20f7e12add63854caeb618
Roshdy <roshdy89@gmail.com>
2017-10-02 15:00:10 +0300

    Update
    - Added method to clear notifications given a query
    - Changed package name

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'raix:push',
-  version: '3.0.3-rc.7',
+  name: 'roshdy:push',
+  version: '3.0.4',
   summary: 'Isomorphic Push notifications for APN and GCM',
-  git: 'https://github.com/raix/push.git'
+  git: 'https://github.com/Roshdy/push.git'
 });
 
 // Server-side push deps
